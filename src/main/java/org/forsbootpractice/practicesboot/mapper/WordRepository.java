2776af049a436861785d9d7188d8ba9f5bcf85da
goodgood619 <gktgnjftm@naver.com>
2020-04-01 01:17:43 +0900

    랜섬웨어후 테스트커밋

--- src/main/java/org/forsbootpractice/practicesboot/mapper/WordRepository.java
+++ src/main/java/org/forsbootpractice/practicesboot/mapper/WordRepository.java
@@ -3,8 +3,6 @@ package org.forsbootpractice.practicesboot.mapper;
 import org.forsbootpractice.practicesboot.dto.Toeic;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface WordRepository extends JpaRepository<Toeic,Long> {
 
 }
