4f26e58205e62cf729e41c4884faa2bb2fda4aff
YoonSeok Lee <52606813+goodgood619@users.noreply.github.com>
2022-04-15 11:59:29 +0900

    - google Oauth 프로필 정보 받아오기 - SecurityConfig에서 userEndPoint 등록해줘야함 - 다양한 계정 (google, kakao, naver, ...)들을 구분하기 위한 userEntity 수정

--- src/main/java/com/example/springsecuritybasic/config/SecurityConfig.java
+++ src/main/java/com/example/springsecuritybasic/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.springsecuritybasic.config;
 
+import com.example.springsecuritybasic.config.oauth.PrincipalOauth2UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -7,12 +9,18 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//1. 코드 받기(인증) 2. 액세스 토큰 (권한) ,
+// 3. 사용자 프로필 정보 가져와서 4-1. 그 정보를 토대로 회원가입을 자동으로 진행시키기도 함
+// 4-2 (이메일, 전화번호, 이름, 아이디) 쇼핑몰 -> (집주소), 백화점몰 -> (vip등급, 일반등급)
 
 @Configuration
 @EnableWebSecurity // Spring Security filter가 Spring Filter Chain에 등록되게 된다
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 // default : false, @Secured 활성화, ex) @Secured(ROLE 명시), default : false, @preAuthorize, @postAuthorize 활성화
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Autowired
+  private PrincipalOauth2UserService principalOauth2UserService;
 
   // 해당 메소드의 리턴되는 object를 IoC로 등록해준다
   @Bean
@@ -26,17 +34,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     http.authorizeRequests()
         .antMatchers("/user/**").authenticated() // 인증이 필요
         .antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('Role_MANAGER')")
-        .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
-        .anyRequest().permitAll()
-        .and()
-        .formLogin()
-        .loginPage("/loginForm")
+            .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+            .anyRequest().permitAll()
+            .and()
+            .formLogin()
+            .loginPage("/loginForm")
 //        .usernameParameter("username2") // controller로 들어오는 username의 변수명이 username2 일때 username으로 매핑해주는 곳임
-        .loginProcessingUrl("/login") // login 주소가 호출이 되면 Spring Security가 낚아채서 대신 로그인을 진행한다
-        .defaultSuccessUrl("/")
-        .and()
-        .oauth2Login()
-        .loginPage("/loginForm"); // google 로그인이 완료된 뒤의 후처리가 필요함
+            .loginProcessingUrl("/login") // login 주소가 호출이 되면 Spring Security가 낚아채서 대신 로그인을 진행한다
+            .defaultSuccessUrl("/")
+            .and()
+            .oauth2Login()
+            .loginPage("/loginForm")
+            .userInfoEndpoint()
+            .userService(principalOauth2UserService); // google 로그인이 완료된 뒤의 후처리가 필요함. tip) 코드 X, accessToken + 사용자프로필정보를 받는다
   }
 
 }
