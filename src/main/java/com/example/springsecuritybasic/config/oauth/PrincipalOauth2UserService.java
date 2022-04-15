9e8789a6e4d7e057c402a7a9eae1df87690d1110
YoonSeok Lee <52606813+goodgood619@users.noreply.github.com>
2022-04-15 16:34:39 +0900

    - naver OAuth2 구현

--- src/main/java/com/example/springsecuritybasic/config/oauth/PrincipalOauth2UserService.java
+++ src/main/java/com/example/springsecuritybasic/config/oauth/PrincipalOauth2UserService.java
@@ -3,9 +3,11 @@ package com.example.springsecuritybasic.config.oauth;
 import com.example.springsecuritybasic.config.auth.PrincipalDetails;
 import com.example.springsecuritybasic.config.oauth.provider.FacebookUserInfo;
 import com.example.springsecuritybasic.config.oauth.provider.GoogleUserInfo;
+import com.example.springsecuritybasic.config.oauth.provider.NaverUserInfo;
 import com.example.springsecuritybasic.config.oauth.provider.OAuth2UserInfo;
 import com.example.springsecuritybasic.model.User;
 import com.example.springsecuritybasic.repository.UserRepository;
+import java.util.Map;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
@@ -42,6 +44,10 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
         } else if (userRequest.getClientRegistration().getRegistrationId().equals("facebook")) {
             System.out.println("페이스북 로그인 요청");
             oAuth2UserInfo = new FacebookUserInfo(oAuth2User.getAttributes());
+        } else if (userRequest.getClientRegistration().getRegistrationId().equals("naver")) {
+            System.out.println("네이버 로그인 요청");
+            oAuth2UserInfo = new NaverUserInfo(
+                (Map<String, Object>) oAuth2User.getAttributes().get("response"));
         } else {
             System.out.println("구글, 페이스북 로그읜이 아닙니다");
         }
@@ -56,7 +62,7 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
         User userEntity = userRepository.findByUsername(userName);
 
         if (userEntity == null) {
-            System.out.println("로그인 최초입니다");
+            System.out.println("OAuth 로그인 최초입니다");
             userEntity = User.builder()
                 .username(userName)
                 .password(password)
